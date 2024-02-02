e707fb9344ebb8a2b0f698029364e672b8e86327
EvertonLDS815 <evertonlino1234@gmail.com>
2024-02-02 18:06:47 -0300

    feat: remove app.use(req.headers)

--- src/index.js
+++ src/index.js
@@ -16,12 +16,6 @@ const corsOptions = {
 };
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // '*' permite qualquer origem, você pode especificar origens específicas
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
-    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Cabeçalhos permitidos
-    next();
-});
 
 app.use(router);
 
